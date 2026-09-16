3f3ee717738e2db9213bb9e3d67230ede16a92ce
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-09-16 14:14:31 -0300

    Criando Pokemon.ts

--- src/components/ShowPokemon/ShowPokemon.tsx
+++
@@ -1,9 +0,0 @@
-import Pokemon from "@/interface/Pokemon";
-import { Image } from "expo-image";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-
-interface ShowPokemonProps {
-    pokemon: Pokemon;
-}export default function ShowPokemon({ pokemon }: ShowPokemonProps) {
-
-}
